c8f13651ae199177da753c9f820f5f5c01d315a2
karlitoyo <karltimmins@gmail.com>
2025-03-11 21:18:34 +0000

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-      return config;
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
   
