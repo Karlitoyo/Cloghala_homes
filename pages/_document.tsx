548eaaa5d2338a297cdff14db904e779053fa424
karlitoyo <karltimmins@gmail.com>
2025-04-02 19:43:48 +0100

    added absolute image url to meta field

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta property="og:title" content="Cloghala Homes" />
         <meta property="og:description" content="A trusted name in plumbing and mechanical services, Cloghala Homes provides quality, innovation, and reliability to the building sector." />
-        <meta property="og:image" content="/images/homePage/Logo/logo.jpg" />
+        <meta property="og:image" content="https://ik.imagekit.io/yaugjs8a5/WhatsApp%20Image%202025-01-10%20at%2008.28.24_85d5578c.jpeg?updatedAt=1743619299283" />
         <meta property="og:url" content="https://www.cloghala.ie/" />
         <meta property="og:type" content="website" />
         <meta name="robots" content="index, follow" />
