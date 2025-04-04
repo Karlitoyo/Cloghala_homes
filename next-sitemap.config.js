f2974fb58bf6509cac43f77a2e6c8c0162e23b19
karlitoyo <karltimmins@gmail.com>
2025-04-04 21:23:21 +0100

    updated sitemap.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,9 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://cloghala.ie',
-  generateRobotsTxt: true,
-};
+  changefreq: 'daily',
+  priority: 0.7,
+  generateRobotsTxt: true, // optional
+  // ...other configs
+  sitemapSize: 7000, // the maximum number of URLs you want in each sitemap file. The library will split urls into multiple sitemaps if the number of urls exceeds this number.
+  generateSitemapIndex: true // set this to true so that the library generates a sitemap index file too.
+}
