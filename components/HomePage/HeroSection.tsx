b715c0acea27cdbd4bef5fb926ab75fda1bf281b
karlitoyo <karltimmins@gmail.com>
2025-04-05 21:01:02 +0100

    commented out htaccess file.

--- components/HomePage/HeroSection.tsx
+++ components/HomePage/HeroSection.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 const HeroSection = () => (
   <section className="relative flex flex-col items-center justify-center w-full h-screen p-10">
-    {/* <div className="absolute inset-0 w-full h-full">
+    <div className="absolute inset-0 w-full h-full">
       <Image className="opacity-25"
         src="/images/homePage/HeroSection/HeroSectionImage.jpg"
         alt="Background"
         layout="fill"
         objectFit="cover"
       />
-    </div> */}
+    </div>
     {/* Overlay for better text visibility */}
     <div className="absolute inset-0 bg-black bg-opacity-40"></div>
 
